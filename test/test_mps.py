748993333d0c6a796b0decf319c82705e80b519b
Sun, Jiayi <jiayi.sun@intel.com>
2023-05-17 11:33:40 +0800

    add Half support for interpolate operators on CPU
    ghstack-source-id: 4de15e0048d0b48b447c119af9071525ff90a11d
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/101530
    
    Conflicts:
    	test/test_mps.py
    	torch/testing/_internal/common_methods_invocations.py

--- test/test_mps.py
+++ test/test_mps.py
@@ -10919,6 +10919,9 @@ class TestConsistency(TestCaseMPS):
         'nn.functional.glu',
         '_native_batch_norm_legit',
         'native_batch_norm',
+        'nn.functional.interpolate',
+        'nn.functional.upsample_bilinear',
+        'nn.functional.upsample_nearest',
 
         # for macOS 12
         'masked.normalize', 'masked.sum', 'masked.var',
