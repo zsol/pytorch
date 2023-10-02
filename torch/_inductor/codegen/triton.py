661c35edc12fc5865f81406e912afd7bf966e876
Peter Bell <peterbell10@live.co.uk>
2023-10-02 17:14:29 +0100

    [inductor] Avoid bool being upcast to int
    Currently the inductor code for `x.any(-1)` does a this strange dance:
    ```python
    tmp0 = tl.load(in_ptr0 + (r1 + (128*x0)), rmask & xmask)
    tmp1 = tmp0.to(tl.int64)
    tmp2 = (tmp1 != 0)
    ```
    
    This happens because `register_lowering` is doing type promotion with the
    dimension argument, and so promotes to `int64` which we then cast back to bool.
    A better fix would be to fix `register_lowering` but for now I just remove
    the unnecessary type promotion from `aten.any`.
    
    In the current code we also see:
    ```python
         tmp5 = tl.where(rmask & xmask, tmp3, 0)
    ```
    which promotes the boolean value to int since `0` is an int32 in triton.
    This fixes it to generate a boolean constant instead.
    
    Finally there is also a triton bug where the `tl.load` itself upcasts to
    `tl.int8`. I fix this by adding an explicit cast to `tl.int1`. The final
    kernel code looks like:
    
    ```python
    tmp0 = tl.load(in_ptr0 + (r1 + (128*x0)), rmask & xmask).to(tl.int1)
    tmp1 = tl.broadcast_to(tmp0, [XBLOCK, RBLOCK])
    tmp3 = tl.full([1, 1], 0, tl.int1)
    tmp4 = tl.where(rmask & xmask, tmp1, tmp3)
    tmp5 = triton_helpers.any(tmp4, 1)[:, None]
    
    ```
    
    ghstack-source-id: d925099de544f22df1553cedb4758f5aa1d35af6
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/109913

--- torch/_inductor/codegen/triton.py
+++ torch/_inductor/codegen/triton.py
@@ -1556,8 +1556,15 @@ class TritonKernel(Kernel):
             default = self._map_tuple_or_scalar(triton_constant, default)
 
             def _mask_value(value, default):
+                ttype = triton_compute_type(src_dtype)
+                # TODO: int1 seems to be broken on triton-rocm
+                ttype = ttype.replace("int1", "int8")
+                other = self.cse.generate(
+                    self.compute,
+                    f"tl.full({[1] * self.triton_tensor_ndim()}, {default}, {ttype})",
+                )
                 return self.cse.generate(
-                    self.compute, f"tl.where({cond}, {value}, {default})"
+                    self.compute, f"tl.where({cond}, {value}, {other})"
                 )
 
             if isinstance(value, tuple):
