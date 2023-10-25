9522817c51733cf6d7acdd0ced8422545b48d751
Catherine Lee <csl@fb.com>
2023-10-25 16:47:41 -0700

    udpate

--- torch/testing/_internal/common_utils.py
+++ torch/testing/_internal/common_utils.py
@@ -1198,6 +1198,7 @@ def xfailIfTorchDynamo(func):
 
 
 def skipIfTorchDynamo(msg="test doesn't currently work with dynamo"):
+    # useless change
     def decorator(fn):
         if not isinstance(fn, type):
             @wraps(fn)
